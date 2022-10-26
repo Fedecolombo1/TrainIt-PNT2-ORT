fec619f999e9bf9bed8a7d8a570a760739f2a4d4
FedeColombo <colombofederico17@gmail.com>
2022-10-26 16:02:44 -0300

    arreglando errores y nuevas vistas

---
+++ proyecto/src/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+
+function HomeScreen() {
+  return (
+    <View>
+        <View id="menuToggle">
+              <TextInput type="checkbox" />
+
+              <View></View>
+              <View></View>
+              <View></View>
+
+        </View>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const style = StyleSheet.create({
+    
+});
