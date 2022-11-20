2e368e88b8f75f71b14f1d66636daf943f81ddf7
naachovega <igna.vega@hotmail.com>
2022-11-20 01:43:57 -0300

    Se modifico el componente de textinput para recibir por prop el tipo de input que levanta el teclado

--- proyecto/Components/TextInput.js
+++ proyecto/Components/TextInput.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { View, TextInput, StyleSheet } from 'react-native'
 
-function CustomInput({value, setValue, placeholder, secureTextEntry, type}) {
+function CustomInput({ value, setValue, placeholder, secureTextEntry, type }) {
   return (
     <View style={style.container}>
-        <TextInput 
-            keyboardType={type} //'email-address' 'number-pad' 'default'
-            style={style.input} 
-            placeholder={placeholder}
-            value={value}
-            onChangeText={setValue}
-            secureTextEntry={secureTextEntry} 
-        />
+      <TextInput
+        keyboardType={type} //'email-address' 'number-pad' 'default'
+        style={style.input}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={setValue}
+        secureTextEntry={secureTextEntry}
+      />
     </View>
   )
 }
@@ -19,17 +19,17 @@ function CustomInput({value, setValue, placeholder, secureTextEntry, type}) {
 export default CustomInput
 
 const style = StyleSheet.create({
-    input: {
-      fontSize: 20,
-      padding: 10,
-    },
-    container:{
-        backgroundColor: '#fff',
-        width: '100%',
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: '#e8e8e8',
-        marginTop: 2,
-        marginBottom: 2,
-    }
-  });
+  input: {
+    fontSize: 20,
+    padding: 10,
+  },
+  container: {
+    backgroundColor: '#fff',
+    width: '100%',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#e8e8e8',
+    marginTop: 2,
+    marginBottom: 2,
+  }
+});
