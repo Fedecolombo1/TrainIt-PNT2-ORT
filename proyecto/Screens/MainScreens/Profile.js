9359f1b0fc7f02ff0f612e1b65f4e8a71a28e84d
naachovega <igna.vega@hotmail.com>
2022-11-25 00:57:08 -0300

    Se agregaron datos al profile

--- proyecto/Screens/MainScreens/Profile.js
+++ proyecto/Screens/MainScreens/Profile.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import CustomButton from "../../Components/CustomButton";
 import AuthContext from "../../Services/AuthContext";
 
@@ -7,7 +7,7 @@ import AuthContext from "../../Services/AuthContext";
 export default function Profile() {
 
     const { user, setUser } = useContext(AuthContext)
-    
+
 
     const signOut = () => {
         setUser(null)
@@ -33,15 +33,24 @@ export default function Profile() {
     }
 
     return (
-
+//TODO: Agregar fecha en la que lo agregaron al team
         <View style={style.container}>
             <View style={style.datosBox}>
+                <Image
+                    style={{ 
+                        width: 100, 
+                        height: 100, 
+                        marginLeft: 20,
+                        marginBottom: 20,
+                        borderRadius: 50, }}
+                    source={{uri:'https://lh3.googleusercontent.com/a/ALm5wu1DNkBpjYLdK4aj-fncx-7yQrwffKINAtP0IpRHTQ=s96-c'}}
+                />
                 <Text style={style.text}>Nombre: {user.nombre}</Text>
                 <Text style={style.text}>Apellido: {user.apellido}</Text>
+                <Text style={style.text}>{user.aptoFisico ? "Apto Fisico al dia" : "Renovar el apto fisico" }</Text>
+                <Text style={style.text}>Cantidad clases: {user.clases.length }</Text>
                 <Text style={style.text}>Edad: {getAge()}</Text>
             </View>
-
-
             <CustomButton text="Sign out" onPress={signOut}></CustomButton>
 
         </View>
@@ -53,10 +62,11 @@ const style = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "space-between",
+        alignItems: 'center',
         padding: 20,
         height: '100%'
     },
-    datosBox:{
+    datosBox: {
         marginTop: 60
     },
     text: {
