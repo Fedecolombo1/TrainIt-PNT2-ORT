6d73c9e97484e3f1b911adbe4dca9f1dd37818f0
Leo Buezo <leonardo@cuddly.com>
2022-11-26 00:49:58 -0300

    resuelvo conflictos de merge

--- proyecto/Services/Clases.js
+++ proyecto/Services/Clases.js
@@ -2,11 +2,7 @@ import { Hostname, PortNumber } from '../config';
 
 export const getClases = () => {
     return new Promise((resolve, reject) => {
-<<<<<<< HEAD
         fetch('http://192.168.0.120:3000/training_class/clases')
-=======
-        fetch(`${Hostname}:${PortNumber}/training_class/clases`)
->>>>>>> origin
             .then((response) => response.json())
             .then(data => {
                 return resolve(data)
